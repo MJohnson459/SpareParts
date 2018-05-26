624ae916d31908e9a1c1373e57bc21ec46ceb5ec
Michael Johnson <mjohnson459@gmail.com>
2018-05-26 11:25:11 +0100

    Refactored borg to use enum

--- src/marvin/src/main.rs
+++ src/marvin/src/main.rs
@@ -4,34 +4,64 @@ extern crate i2cdev;
 mod picoborg_rev;
 
 use tiny_http::{Server, Response};
-#[cfg(any(target_os = "linux", target_os = "android"))]
-use i2cdev::linux::LinuxI2CError;
-use std::error::Error;
+use std::io::Cursor;
 use std::path::Path;
 
-
 use picoborg_rev::PicoBorgRev;
+
+struct SpareParts {
+    borg: Option<PicoBorgRev>,
+}
+
+impl SpareParts {
+    fn new() -> SpareParts {
+        SpareParts {
+            borg: PicoBorgRev::new(Path::new("/dev/i2c-1")).ok(),
+        }
+    }
+
+    fn run(&mut self) {
+        let server = Server::http("0.0.0.0:8000").unwrap();
+
+
+        for request in server.incoming_requests() {
+            println!("received request! method: {:?}, url: {:?}",
+                request.method(),
+                request.url(),
+            );
+
+            let path: Vec<&str> = request.url().split('/').collect();
+
+            let response = match path[0] {
+                "borg" => self.handle_borg(&path.as_slice()[1..]),
+                _ => Response::from_string("Request not recognised"),
+            };
+
+            request.respond(response);
+        }
+
+    }
+
+    fn handle_borg(&mut self, request: &[&str]) -> Response<Cursor<Vec<u8>>> {
+        // Check borg is available
+        match self.borg {
+            Some(borg) => match request[0] {
+                "toggle_led" => {
+                    let led_on = borg.toggle_led();
+                    Response::from_string(format!("led_on: {}", led_on))
+
+                },
+                _ => Response::from_string("Request not recognised"),
+                },
+            None => Response::from_string("PicoBorgRev not available"),
+        }
+    }
+}
+
 
 fn main() {
     println!("Hello, world!");
 
-    let server = Server::http("0.0.0.0:8000").unwrap();
-
-    let mut borg = PicoBorgRev::new(Path::new("/dev/i2c-1")).unwrap();
-
-    for request in server.incoming_requests() {
-        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
-            request.method(),
-            request.url(),
-            request.headers()
-        );
-
-        if request.url() == "/toggle_led" {
-            let response = match borg.toggle_led() {
-                Ok(led_on) => Response::from_string(format!("hello world: {}", led_on)),
-                Err(what) => Response::from_string(what.description()),
-            };
-            request.respond(response);
-        }
-    }
+    let robot = SpareParts::new();
+    robot.run();
 }
--- src/marvin/src/picoborg_rev/mod.rs
+++ src/marvin/src/picoborg_rev/mod.rs
@@ -1,5 +1,5 @@
-use std::thread;
-use std::time::Duration;
+// use std::thread;
+// use std::time::Duration;
 use std::path::Path;
 
 use i2cdev::core::*;
@@ -15,37 +15,40 @@ const I2C_MAX_LEN: u8             = 4;
 
 const I2C_ID_PICOBORG_REV: u8     = 0x15;
 
-const COMMAND_SET_LED: u8         = 1;     // Set the LED status
-const COMMAND_GET_LED: u8         = 2;     // Get the LED status
-const COMMAND_SET_A_FWD: u8       = 3;     // Set motor 2 PWM rate in a forwards direction
-const COMMAND_SET_A_REV: u8       = 4;     // Set motor 2 PWM rate in a reverse direction
-const COMMAND_GET_A: u8           = 5;     // Get motor 2 direction and PWM rate
-const COMMAND_SET_B_FWD: u8       = 6;     // Set motor 1 PWM rate in a forwards direction
-const COMMAND_SET_B_REV: u8       = 7;     // Set motor 1 PWM rate in a reverse direction
-const COMMAND_GET_B: u8           = 8;     // Get motor 1 direction and PWM rate
-const COMMAND_ALL_OFF: u8         = 9;     // Switch everything off
-const COMMAND_RESET_EPO: u8       = 10;    // Resets the EPO flag, use after EPO has been tripped and switch is now clear
-const COMMAND_GET_EPO: u8         = 11;    // Get the EPO latched flag
-const COMMAND_SET_EPO_IGNORE: u8  = 12;    // Set the EPO ignored flag, allows the system to run without an EPO
-const COMMAND_GET_EPO_IGNORE: u8  = 13;    // Get the EPO ignored flag
-const COMMAND_GET_DRIVE_FAULT: u8 = 14;    // Get the drive fault flag, indicates faults such as short-circuits and under voltage
-const COMMAND_SET_ALL_FWD: u8     = 15;    // Set all motors PWM rate in a forwards direction
-const COMMAND_SET_ALL_REV: u8     = 16;    // Set all motors PWM rate in a reverse direction
-const COMMAND_SET_FAILSAFE: u8    = 17;    // Set the failsafe flag, turns the motors off if communication is interrupted
-const COMMAND_GET_FAILSAFE: u8    = 18;    // Get the failsafe flag
-const COMMAND_SET_ENC_MODE: u8    = 19;    // Set the board into encoder or speed mode
-const COMMAND_GET_ENC_MODE: u8    = 20;    // Get the boards current mode, encoder or speed
-const COMMAND_MOVE_A_FWD: u8      = 21;    // Move motor 2 forward by n encoder ticks
-const COMMAND_MOVE_A_REV: u8      = 22;    // Move motor 2 reverse by n encoder ticks
-const COMMAND_MOVE_B_FWD: u8      = 23;    // Move motor 1 forward by n encoder ticks
-const COMMAND_MOVE_B_REV: u8      = 24;    // Move motor 1 reverse by n encoder ticks
-const COMMAND_MOVE_ALL_FWD: u8    = 25;    // Move all motors forward by n encoder ticks
-const COMMAND_MOVE_ALL_REV: u8    = 26;    // Move all motors reverse by n encoder ticks
-const COMMAND_GET_ENC_MOVING: u8  = 27;    // Get the status of encoders moving
-const COMMAND_SET_ENC_SPEED: u8   = 28;    // Set the maximum PWM rate in encoder mode
-const COMMAND_GET_ENC_SPEED: u8   = 29;    // Get the maximum PWM rate in encoder mode
-const COMMAND_GET_ID: u8          = 0x99;  // Get the board identifier
-const COMMAND_SET_I2C_ADD: u8     = 0xAA;  // Set a new I2C address
+#[repr(u8)]
+enum Command {
+    SET_LED         = 1,     // Set the LED status
+    GET_LED         = 2,     // Get the LED status
+    SET_A_FWD       = 3,     // Set motor 2 PWM rate in a forwards direction
+    SET_A_REV       = 4,     // Set motor 2 PWM rate in a reverse direction
+    GET_A           = 5,     // Get motor 2 direction and PWM rate
+    SET_B_FWD       = 6,     // Set motor 1 PWM rate in a forwards direction
+    SET_B_REV       = 7,     // Set motor 1 PWM rate in a reverse direction
+    GET_B           = 8,     // Get motor 1 direction and PWM rate
+    ALL_OFF         = 9,     // Switch everything off
+    RESET_EPO       = 10,    // Resets the EPO flag, use after EPO has been tripped and switch is now clear
+    GET_EPO         = 11,    // Get the EPO latched flag
+    SET_EPO_IGNORE  = 12,    // Set the EPO ignored flag, allows the system to run without an EPO
+    GET_EPO_IGNORE  = 13,    // Get the EPO ignored flag
+    GET_DRIVE_FAULT = 14,    // Get the drive fault flag, indicates faults such as short-circuits and under voltage
+    SET_ALL_FWD     = 15,    // Set all motors PWM rate in a forwards direction
+    SET_ALL_REV     = 16,    // Set all motors PWM rate in a reverse direction
+    SET_FAILSAFE    = 17,    // Set the failsafe flag, turns the motors off if communication is interrupted
+    GET_FAILSAFE    = 18,    // Get the failsafe flag
+    SET_ENC_MODE    = 19,    // Set the board into encoder or speed mode
+    GET_ENC_MODE    = 20,    // Get the boards current mode, encoder or speed
+    MOVE_A_FWD      = 21,    // Move motor 2 forward by n encoder ticks
+    MOVE_A_REV      = 22,    // Move motor 2 reverse by n encoder ticks
+    MOVE_B_FWD      = 23,    // Move motor 1 forward by n encoder ticks
+    MOVE_B_REV      = 24,    // Move motor 1 reverse by n encoder ticks
+    MOVE_ALL_FWD    = 25,    // Move all motors forward by n encoder ticks
+    MOVE_ALL_REV    = 26,    // Move all motors reverse by n encoder ticks
+    GET_ENC_MOVING  = 27,    // Get the status of encoders moving
+    SET_ENC_SPEED   = 28,    // Set the maximum PWM rate in encoder mode
+    GET_ENC_SPEED   = 29,    // Get the maximum PWM rate in encoder mode
+    GET_ID          = 0x99,  // Get the board identifier
+    SET_I2C_ADD     = 0xAA,  // Set a new I2C address
+}
 
 const COMMAND_VALUE_FWD: u8       = 1;     // I2C value representing forward
 const COMMAND_VALUE_REV: u8       = 2;     // I2C value representing reverse
@@ -63,7 +66,7 @@ impl PicoBorgRev {
         let mut device = LinuxI2CDevice::new(i2c_path, I2C_ADDRESS)?;
 
         // Check for PicoBorg Reverse
-        let data = device.smbus_read_word_data(COMMAND_GET_ID)?;
+        let data = device.smbus_read_word_data(Command::GET_ID)?;
         let id = (data >> 8) as u8;
 
         if id != I2C_ID_PICOBORG_REV {
@@ -79,26 +82,10 @@ impl PicoBorgRev {
     pub fn toggle_led(&mut self) -> Result<bool, LinuxI2CError> {
         self.led_on = !self.led_on;
         println!("Toggling led {}", self.led_on);
-        try!(self.device.smbus_write_byte_data(0x01, if self.led_on {0x01} else {0x00}));
+        try!(self.device.smbus_write_byte_data(0x01, if self.led_on {COMMAND_VALUE_ON} else {COMMAND_VALUE_OFF}));
 
         Ok(self.led_on)
     }
-}
-
-
-// real code should probably not use unwrap()
-pub fn toggle_led() -> Result<(), LinuxI2CError> {
-    let mut dev = try!(LinuxI2CDevice::new("/dev/i2c-1", I2C_ADDRESS));
-
-    thread::sleep(Duration::from_millis(100));
-
-    println!("Toggling led on");
-    try!(dev.smbus_write_byte_data(0x01, 0x01));
-    thread::sleep(Duration::from_secs(10));
-    println!("Toggling led off");
-    try!(dev.smbus_write_byte_data(0x01, 0x00)); // On
-
-    Ok(())
 }
 
 #[cfg(test)]
